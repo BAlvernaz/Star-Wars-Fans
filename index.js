2046c19578238bd4b514af75a9f6204ddbe408f1
Blake Alvernaz <balverna@gmail.com>
2019-05-20 06:50:07 -0700

    Added List of Star Wars Charecters

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-console.log('Far Away Galaxy')
-const peopleDiv = document.querySelector('#people')
-
-const testFetch = async () => {
-  for (let i = 1; i < 31; i++)
-    {
-     const response = await fetch(`https://swapi.co/api/people/${i}/`);
-     const people = await response.json();
-   }
-   return people
+console.log('Far Away Galaxy');
+const peopleDiv = document.getElementById('people');
+const peopleDes = document.getElementById('peepInfo')
+const fetchSWPeople = async () => {
+  const arrSWPeople = [];
+  for(let i = 1; i < 31; i++) {
+    const responce = await fetch(`https://swapi.co/api/people/${i}`)
+    const people = await responce.json();
+    arrSWPeople.push(people);
+  }
+  return arrSWPeople;
 }
-testFetch()
+
+const renderPeople = async() => {
+  const people = await fetchSWPeople();
+    createListOfPeople(people)
+}
+
+const createListOfPeople = (people) => {
+  const HTML =  `
+    <ul>
+      ${people.map(elem => `
+      <li class="SWPeople" id=${elem.name}> ${elem.name}</li>`).sort().join('')}
+    </ul>
+  `;
+   peopleDiv.innerHTML = HTML;
+}
+
+renderPeople()
+
+peopleDiv.addEventListener('click', (event) =>{
+  if (event.target.className === "SWPeople") {
+ console.log(event.target)
+}
+});
+
+
+
+
 
